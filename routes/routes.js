2cd162e595d3cfd6ac881c5a1193598fa8343197
suvaghosh <suvaghosh@USCHISUVAGHOSH1.us.deloitte.com>
2019-07-30 10:29:10 -0500

    Modified the return json to deny any request from Okta

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,20 @@
-var faker = require("faker");
 const JSON = require('circular-json');
 
 var appRouter = function (app) {
   app.post("/", function (req, res) {
-    console.log(req.body)
-    res.status(200).send({ message: "Hello" });
+    console.log(req.body.data.user.profile);
+    
+//Send deny command to Okta registration hook
+    res.status(200).json({
+    	   "commands":[
+    		      {
+    		         "type":"com.okta.action.update",
+    		         "value":{
+    		            "registration":"DENY"
+    		         }
+    		      }
+    		   ]
+    		});
   });
 }
 module.exports = appRouter;
