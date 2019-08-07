c8470f19297d5375787e912d4a6631eca7efafb8
Suvadeep90 <suvadeepunplugged@gmail.com>
2019-08-06 22:24:51 -0500

    m

--- okta/oktaroutes.js
+++ okta/oktaroutes.js
@@ -14,10 +14,20 @@ oktapost.post("/createUser",function (req, res) {
         token: '00mS7WX5Hink4vKrAfWq_DoEd98-cGadqgeq-HyztM'    // Obtained from Developer Dashboard
     });
     console.log(req.body);
+    var profile={};
+    var firstName ='firstName';
+    var lastName ='lastName';
+    var email ='email';
+    var mobilePhone ='mobilePhone';
+    profile[firstName] = req.body.firstName;
+    profile[lastName] = req.body.lastName;
+    profile[email] = req.body.email;
+    profile[mobilePhone] = req.body.mobilePhone;
+
     var o = {} // empty Object
     var key = 'Profile';
     o[key] = [];
-    o[key].push(req.body)
+    o[key].push(profile)
     console.log(o);
     requestData=o;
     client.createUser(requestData)
