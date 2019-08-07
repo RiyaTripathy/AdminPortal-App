96bcbc71c7fac753fe69a83f8c47eb6a162bbe34
Suvadeep90 <suvadeepunplugged@gmail.com>
2019-08-06 22:40:04 -0500

    form UI

--- notification/routes.js
+++ notification/routes.js
@@ -36,7 +36,170 @@ router.post("/notify", function (req, res) {
     </form>
 `;
 
+    const formdata = `<!DOCTYPE html>
+    <html>
+      <head>
+        <title>Account registration form</title>
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600' rel='stylesheet' type='text/css'>
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+        <style>
+          html, body {
+          min-height: 100%;
+          }
+          body, div, form, input, select, p { 
+          padding: 0;
+          margin: 0;
+          outline: none;
+          font-family: Roboto, Arial, sans-serif;
+          font-size: 14px;
+          color: #666;
+          }
+          h1 {
+          margin: 0;
+          font-weight: 400;
+          }
+          h3 {
+          margin: 12px 0;
+          color: #FF0000;
+          }
+          .main-block {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          background: #fff;
+          }
+          form {
+          width: 100%;
+          padding: 20px;
+          }
+          fieldset {
+          border: none;
+          border-top: 1px solid #8ebf42;
+          }
+          .account-details, .personal-details {
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: space-between;
+          }
+          .account-details >div, .personal-details >div >div {
+          display: flex;
+          align-items: center;
+          margin-bottom: 10px;
+          }
+          .account-details >div, .personal-details >div, input, label {
+          width: 100%;
+          }
+          label {
+          padding: 0 5px;
+          text-align: right;
+          vertical-align: middle;
+          }
+          input {
+          padding: 5px;
+          vertical-align: middle;
+          }
+          .checkbox {
+          margin-bottom: 10px;
+          }
+          select, .children, .gender, .bdate-block {
+          width: calc(100% + 26px);
+          padding: 5px 0;
+          }
+          select {
+          background: transparent;
+          }
+          .gender input {
+          width: auto;
+          } 
+          .gender label {
+          padding: 0 5px 0 0;
+          } 
+          .bdate-block {
+          display: flex;
+          justify-content: space-between;
+          }
+          .birthdate select.day {
+          width: 35px;
+          }
+          .birthdate select.mounth {
+          width: calc(100% - 94px);
+          }
+          .birthdate input {
+          width: 38px;
+          vertical-align: unset;
+          }
+          .checkbox input, .children input {
+          width: auto;
+          margin: -2px 10px 0 0;
+          }
+          .checkbox a {
+          color: #8ebf42;
+          }
+          .checkbox a:hover {
+          color: #82b534;
+          }
+          button {
+          width: 100%;
+          padding: 10px 0;
+          margin: 10px auto;
+          border-radius: 5px; 
+          border: none;
+          background: #FF0000; 
+          font-size: 14px;
+          font-weight: 600;
+          color: #FFFFFF;
+          }
+          button:hover {
+          background: #82b534;
+          }
+          @media (min-width: 568px) {
+          .account-details >div, .personal-details >div {
+          width: 50%;
+          }
+          label {
+          width: 40%;
+          }
+          input {
+          width: 60%;
+          }
+          select, .children, .gender, .bdate-block {
+          width: calc(60% + 16px);
+          }
+          }
+        </style>
+      </head>
+      <body>
+        <div class="main-block">
+        <form action="http://ec2-3-17-73-62.us-east-2.compute.amazonaws.com:3000/okta/createUser">
+          <h1>New User account creation</h1>
+          <fieldset>
+            <legend>
+              <h3>Account Details</h3>
+            </legend>
+            <div  class="account-details">
+              <div><label>Email*</label><input type="text" name="email" value="${req.body.data.user.profile.email}" required></div>
+              <div><label>Login*</label><input type="text" name="login" value="${req.body.data.user.profile.login}" required></div>
+            </div>
+          </fieldset>
+          <fieldset>
+            <legend>
+              <h3>Personal Details</h3>
+            </legend>
+            <div  class="personal-details">
+              <div>
+                <div><label>First Name*</label><input type="text" name="firstName" value="${req.body.data.user.profile.firstName}" required></div>
+                <div><label>Last Name*</label><input type="text" name="lastName" value="${req.body.data.user.profile.lastName}" required></div>
+              </div>
+          </fieldset>
+          <button type="submit">Approve</button>
+        </form>
+        </div> 
+      </body>
+    </html>`;
+    
+    
 
+    
     const form=`<!DOCTYPE html>
     <html>
     <head>
@@ -45,7 +208,7 @@ router.post("/notify", function (req, res) {
     </head>
     <body>
     <div id="contact">
-        <h1>Send an email</h1>
+        <h1>New User Request</h1>
         <form action="http://ec2-3-17-73-62.us-east-2.compute.amazonaws.com:3000/okta/createUser" method="post">
             <fieldset>
                 <label for="name">First Name:</label>
@@ -90,7 +253,7 @@ router.post("/notify", function (req, res) {
         to: 's.ghosh3671@gmail.com',
         subject: `New User Request - ${req.body.data.user.profile.firstName} ${req.body.data.user.profile.lastName}`,
         text: 'None',
-        html: form
+        html: formdata
     };
 
     transport.sendMail(mailOptions, (error, info) => {
--- okta/oktaroutes.js
+++ okta/oktaroutes.js
@@ -13,23 +13,7 @@ oktapost.post("/createUser",function (req, res) {
         orgUrl: 'https://halliburton.oktapreview.com/',
         token: '00mS7WX5Hink4vKrAfWq_DoEd98-cGadqgeq-HyztM'    // Obtained from Developer Dashboard
     });
-    console.log(req.body);
-    var profile={};
-    var firstName ='firstName';
-    var lastName ='lastName';
-    var email ='email';
-    var mobilePhone ='mobilePhone';
-    profile[firstName] = req.body.firstName;
-    profile[lastName] = req.body.lastName;
-    profile[email] = req.body.email;
-    profile[mobilePhone] = req.body.mobilePhone;
-
-    var o = {} // empty Object
-    var key = 'Profile';
-    o[key] = [];
-    o[key].push(profile)
-    console.log(o);
-    requestData=o;
+    requestData="";
     client.createUser(requestData)
         .then(user => {
             console.log('Created user', user);
