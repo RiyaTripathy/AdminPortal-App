87b5b37a2f672ab22effd3c523361e53afac4bb5
Suvadeep90 <suvadeepunplugged@gmail.com>
2019-08-06 22:43:11 -0500

    Clean up code

--- notification/routes.js
+++ notification/routes.js
@@ -18,24 +18,6 @@ router.post("/notify", function (req, res) {
     		   ]
     		});
 
-    const output = `
-        <p>You have a new user request</p>
-        <h3>User Details</h3>
-        <ul>
-            <li>First Name: ${req.body.data.user.profile.firstName}</li>
-            <li>Last Name: ${req.body.data.user.profile.lastName}</li>
-            <li>Email: ${req.body.data.user.profile.email}</li>
-            <li>Login: ${req.body.data.user.profile.login}</li>
-            <li>Justification: ${req.body.data.user.profile.justification}</li>
-            <li>Phone: ${req.body.data.user.profile.mobilePhone}</li>
-        </ul>
-        <a href="http://ec2-3-17-73-62.us-east-2.compute.amazonaws.com:3000/okta/info">Approve here</a>
-        <form action="http://ec2-3-17-73-62.us-east-2.compute.amazonaws.com:3000/okta/info" method="get">
-        <input type="submit" value="Approve" 
-             name="Approve" id="frm1_submit" />
-    </form>
-`;
-
     const formdata = `<!DOCTYPE html>
     <html>
       <head>
@@ -170,7 +152,7 @@ router.post("/notify", function (req, res) {
       </head>
       <body>
         <div class="main-block">
-        <form action="http://ec2-3-17-73-62.us-east-2.compute.amazonaws.com:3000/okta/createUser">
+        <form action="http://ec2-3-17-73-62.us-east-2.compute.amazonaws.com:3000/okta/createUser" method="post">
           <h1>New User account creation</h1>
           <fieldset>
             <legend>
@@ -200,42 +182,6 @@ router.post("/notify", function (req, res) {
     
 
     
-    const form=`<!DOCTYPE html>
-    <html>
-    <head>
-    <meta charset="utf-8" />
-    <title>CSS3 Contact Form</title>
-    </head>
-    <body>
-    <div id="contact">
-        <h1>New User Request</h1>
-        <form action="http://ec2-3-17-73-62.us-east-2.compute.amazonaws.com:3000/okta/createUser" method="post">
-            <fieldset>
-                <label for="name">First Name:</label>
-                <input type="text" id="firstName" name="firstName" value= "${req.body.data.user.profile.firstName}"/>
-
-                <label for="name">Last Name:</label>
-                <input type="text" id="lastName" name="lastName" value= "${req.body.data.user.profile.lastName}"/>
-
-                <label for="name">Login:</label>
-                <input type="text" id="login" name="login" value= "${req.body.data.user.profile.login}"/>
-
-                <label for="name">Email:</label>
-                <input type="text" id="email" name="email" value= "${req.body.data.user.profile.email}"/>
-
-                <label for="name">Phone Number:</label>
-                <input type="text" id="mobilePhone" name="mobilePhone" value= "${req.body.data.user.profile.mobilePhone}"/>
-    
-                <label for="message">Justification:</label>
-                <textarea id="justification" value= "${req.body.data.user.profile.justification}" placeholder="What's on your mind?"></textarea>
-    
-                <input type="submit" value="Approve" />
-    
-            </fieldset>
-        </form>
-    </div>
-    </body>
-    </html>`;
 
     let transport = nodemailer.createTransport({
         host: 'smtp.gmail.com',
